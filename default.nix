1cbbca8f8f1a28ae7ac661b7f8f53c0eeb303e5d
Tristan Cacqueray <tdecacqu@redhat.com>
2021-04-24 02:43:19 +0000

    Add database service

--- default.nix
+++ default.nix
@@ -6,12 +6,36 @@ let
     sha256 = "0r00azbz64fz8yylm8x37imnrsm5cdzshd5ma8gwfwjyw166n3r1";
   }));
   pkgs = nixpkgsSrc { };
+  pkgsNonFree = nixpkgsSrc { config.allowUnfree = true; };
 
 in let
   fri = rec {
+    # DB
+    elkConf = pkgs.writeTextFile {
+      name = "elasticsearch.yml";
+      text = ''
+        http.port: ${toString elk-port}
+      '';
+    };
+    startElk = pkgs.writeTextFile {
+      name = "startElk.sh";
+      executable = true;
+      text = ''
+        export ES_HOME=/tmp/es-home
+        mkdir -p $ES_HOME/logs $ES_HOME/data
+        ${pkgs.rsync}/bin/rsync -a ${elk}/config/ $ES_HOME/config/
+        ln -sf ${elk}/modules $ES_HOME/modules 2> /dev/null
+        find $ES_HOME -type f | xargs chmod 0600
+        find $ES_HOME -type d | xargs chmod 0700
+        cat ${elkConf} > $ES_HOME/config/elasticsearch.yml
+        exec ${elk}/bin/elasticsearch
+      '';
+    };
+    elk = pkgsNonFree.elasticsearch7;
+
     # Debug
     debug = pkgs.mkShell {
-      inputsFrom = [ ];
+      inputsFrom = [ elk ];
       buildInputs = [ pkgs.strace ];
     };
   };
